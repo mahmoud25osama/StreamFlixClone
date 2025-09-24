009993c962c93e445cc2170980f9441505f1ea5a
mahmoud25osama <mahmoud4h5@gmail>
2025-09-24 15:37:33 +0300

    u

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-// https://vite.dev/config/
+import { fileURLToPath } from 'url'
+
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src'),
+            '@': path.resolve(
+                path.dirname(fileURLToPath(import.meta.url)),
+                './src'
+            ),
         },
     },
 })
